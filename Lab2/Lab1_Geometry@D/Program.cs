4dba9a51ff0b24054300d11dc3e47c57f5cba31e
hoshmandeep <78502348+hoshmandeep@users.noreply.github.com>
2021-09-30 00:54:52 -0400

    lab2

---
+++ Lab2/Lab1_Geometry@D/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Lab1_Geometry2D.Geometry2D;
+using Lab1_Geometry2D;
+using Lab1_Geometry2D.Particles;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phonon p = new Phonon(1);
+
+            Phonon p1 = new Phonon(p); // p1 will be a replica of p
+
+          
+
+        }
+    }
+}
